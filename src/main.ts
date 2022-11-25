6f1e6f475cdb8a07838e8f14693078cea77b1caf
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-25 17:54:31 +0100

    chore(main): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('BookmarkApi')
+    .setDescription('Nestjs Bookmark tuto project API')
+    .setVersion('1.0')
+    .addTag('bookmark')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
