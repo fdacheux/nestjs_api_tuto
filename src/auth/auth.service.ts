197ac87d418ca3a051adb20a116787f091c1f750
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-01 11:22:39 +0100

    feat(strategy): create jwt auth strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  async signup(dto: AuthDto) {
+  signup = async (dto: AuthDto) => {
     //Generate password
     const hash = await argon.hash(dto.password);
     // Save the new user in db
@@ -21,9 +27,8 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-      // Return the saved user
-      return user;
+      //send back token
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -32,9 +37,9 @@ export class AuthService {
       }
       throw error;
     }
-  }
+  };
 
-  async signin(dto: AuthDto) {
+  signin = async (dto: AuthDto) => {
     //find the user by email
     const user = await this.prisma.user.findUnique({
       where: {
@@ -49,8 +54,30 @@ export class AuthService {
     //if passwords incorrect, throw err
     if (!passwordMatches) throw new ForbiddenException('Credentials incorrect');
 
-    //send back user
-    delete user.hash;
-    return user;
-  }
+    //send back token
+    return this.signToken(user.id, user.email);
+  };
+  signToken = async (
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> => {
+    //Create payload for jwt
+    const payload = {
+      sub: userId,
+      email,
+    };
+    // Get the secret password for JWT from .env file
+    const secret = this.config.get('JWT_SECRET');
+
+    //Prepare token
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    // Return token
+    return {
+      access_token: token,
+    };
+  };
 }
